fba5da0174e3b557f8e6be7e96c7aa422db47067
Er. Mohammed Jaseem <mail@jassy.in>
2025-02-13 22:33:52 +0530

    Updated

--- gate_pass/urls.py
+++ gate_pass/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import apply_staff_hostel_gate_pass
+from . import views
 
 urlpatterns = [
-    path('apply-hostel-staff-gate-pass/', apply_staff_hostel_gate_pass, name='apply_staff_gate_pass'),
+    path('apply-hostel-staff-gate-pass/', views.apply_staff_hostel_gate_pass, name='apply_staff_gate_pass'),
+    path('get_my_pass_list', views.get_my_pass_list, name="get_my_pass_list"),
+    path('Hostel_gatepass/<str: token>/<str: decision>/', views.HostelStaffGatePassApprove, name="get_my_pass_list"),
 ]
