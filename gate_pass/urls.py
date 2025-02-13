c5404a0f717e171781557f72240ae14361ba1815
Er. Mohammed Jaseem <mail@jassy.in>
2025-02-13 22:35:03 +0530

    updated

--- gate_pass/urls.py
+++ gate_pass/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('apply-hostel-staff-gate-pass/', views.apply_staff_hostel_gate_pass, name='apply_staff_gate_pass'),
     path('get_my_pass_list', views.get_my_pass_list, name="get_my_pass_list"),
-    path('Hostel_gatepass/<str: token>/<str: decision>/', views.HostelStaffGatePassApprove, name="get_my_pass_list"),
+    path('Hostel_gatepass/<str:token>/<str:decision>/', views.HostelStaffGatePassApprove, name="Hostel_gatepass_approve"),
 ]
