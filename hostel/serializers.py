79017dc6fdcfe4e1fe5b6de929595597fcb511b1
anwarmusthafa <anwarmusthafa99@gmail.com>
2025-02-17 14:47:09 +0530

    add incharge_name field to HostelSerializer for fetching incharge name

--- hostel/serializers.py
+++ hostel/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
 from .models import Hostel
+from rest_framework import serializers
+from .models import Hostel
 
 class HostelSerializer(serializers.ModelSerializer):
+    incharge_name = serializers.CharField(source='incharge.name', read_only=True)  # Fetch incharge name
+
     class Meta:
         model = Hostel
-        fields = '__all__' 
+        fields = '__all__'  # Ensuring all fields are included, including `incharge_name`
