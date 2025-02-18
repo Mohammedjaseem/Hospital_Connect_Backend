c25d091ed38009427b7939b9fd4ce8db5db7ab6e
Er. Mohammed Jaseem <mail@jassy.in>
2025-02-18 15:32:21 +0530

    updated

--- utils/whatsapp_sender.py
+++ utils/whatsapp_sender.py
@@ -41,7 +41,11 @@ def send_whatsapp_message(request, passing_data, type, sent_to):
                 return True
             except Exception as e:
                 print(f"Error updating bill: {e}")
-            return True
+                return {
+                        "status": "error",
+                        "message": "Failed to send message",
+                        "details": response.text,
+                    }
         else:
             print("response.text: ",response.text)
             return {
