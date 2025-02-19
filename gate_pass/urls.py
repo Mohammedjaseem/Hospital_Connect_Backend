66c617307be8bf2ea366c92502f162d53defb68c
anwarmusthafa <anwarmusthafa99@gmail.com>
2025-02-19 14:46:07 +0530

    feat: add endpoint to retrieve approved gate passes for mentor

--- gate_pass/urls.py
+++ gate_pass/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('get_my_pass_list/', views.get_my_pass_list, name="get_my_pass_list"),
     path('mentor_approval_pendings/', views.mentor_approval_pendings, name="mentor_approval_pendings"),
     path('mentor_rejected_gate_passes/', views.mentor_rejected_gate_passes, name="mentor_rejected_gate_passes"),
+    path('mentor_approved_gate_passes/', views.mentor_approved_gate_passes, name="mentor_approved_gate_passes"),
+
 
     path('Hostel_gatepass/<str:token>/<str:decision>/', views.HostelStaffGatePassApprove, name="Hostel_gatepass_approve"),
     path('check_in_check_out_marker/', views.check_in_check_out_marker, name="check_in_check_out_marker"),
--- gate_pass/views.py
+++ gate_pass/views.py
@@ -376,6 +376,31 @@ def mentor_rejected_gate_passes(request):
 
     except Exception as e:
         return handle_exception(e)
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def mentor_approved_gate_passes(request):
+    try:
+        # Get staff profile
+        staff_profile = get_staff_profile(request)
+
+        # Get gate passes for the staff
+        gatepasses = HostelStaffGatePass.objects.filter(mentor=staff_profile, request_status="Approved")
+
+        # Serialize gate passes with pagination
+        paginated_response = paginate_and_serialize(gatepasses, request, HostelStaffGatePassSerializer, 70)
+
+        # Extract the paginated data
+        paginated_data = paginated_response.data  # Extracting data from the Response object
+
+        return Response({
+            "status": True,
+            "message": "Gate passes retrieved successfully",
+            "data": paginated_data
+        }, status=paginated_response.status_code)  # Maintain the original status code
+
+    except Exception as e:
+        return handle_exception(e)
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
