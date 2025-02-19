766b79c30a3e046dd8268483dadde9e6e9458c2e
Er. Mohammed Jaseem <mail@jassy.in>
2025-02-19 12:07:27 +0530

    uopdated

--- administration/admin.py
+++ administration/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from administration.models import Departments, Designations
 
-# Register your models here.
+@admin.register(Departments)
+class DepartmentsAdmin(admin.ModelAdmin):
+    list_display = ("short_code", "name", "phone_number", "email", "hod", "incharge", "created_at", "updated_at")
+    search_fields = ("name", "short_code", "phone_number", "email")
+    list_filter = ("created_at", "updated_at")
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("name",)
+    
+@admin.register(Designations)
+class DesignationsAdmin(admin.ModelAdmin):
+    list_display = ("name", "department", "created_at", "updated_at")
+    search_fields = ("name", "department__name")
+    list_filter = ("created_at", "updated_at", "department")
+    readonly_fields = ("created_at", "updated_at")
+    ordering = ("name",)
