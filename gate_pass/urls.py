6e990773ac535bfac8c688016ee7f187ca00ae1e
anwarmusthafa <anwarmusthafa99@gmail.com>
2025-02-24 16:06:49 +0530

    feat: update get_pass_list endpoint to use class-based view for improved structure

--- gate_pass/urls.py
+++ gate_pass/urls.py
@@ -3,15 +3,14 @@ from . import views
 
 urlpatterns = [
     path('apply-hostel-staff-gate-pass/', views.apply_staff_hostel_gate_pass, name='apply_staff_gate_pass'),
-    path('get_pass_list/', views.get_my_pass_list, name="get_my_pass_list"),
+    path('get_pass_list/', views.GetPassListView.as_view(), name="get_my_pass_list"),
     path('mentor_approval_pendings/', views.mentor_approval_pendings, name="mentor_approval_pendings"),
     path('mentor_rejected_gate_passes/', views.mentor_rejected_gate_passes, name="mentor_rejected_gate_passes"),
     path('mentor_approved_gate_passes/', views.mentor_approved_gate_passes, name="mentor_approved_gate_passes"),
     path('pass_counts_for_mentor/', views.pass_counts_for_mentor, name="pass_counts_for_mentor"),
-    path('search_staff_pass/', views.search_staff_pass, name="serach_staff_pass"),
+    path('search_staff_pass/', views.search_staff_pass, name="search_staff_pass"),
     path('Hostel_gatepass/<str:token>/<str:decision>/', views.HostelStaffGatePassApprove, name="Hostel_gatepass_approve"),
     path('check_in_check_out_marker/', views.check_in_check_out_marker, name="check_in_check_out_marker"),
     path('gate_pass_report/', views.gate_pass_report, name="gate_pass_report"),
     path("single_pass_data/", views.single_pass_data, name="single_pass_data"),
-
 ]
