23860404df45bdcc78c44a7e923015409b3b9992
anwarmusthafa <anwarmusthafa99@gmail.com>
2025-03-03 14:33:03 +0530

    feat: add DebugMiddleware to log request headers for improved debugging

--- Hospital_Connect/settings.py
+++ Hospital_Connect/settings.py
@@ -326,3 +326,17 @@ LOGGING = {
         },
     },
 }
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class DebugMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        logger.info(f"Headers: {request.headers}")
+        response = self.get_response(request)
+        return response
